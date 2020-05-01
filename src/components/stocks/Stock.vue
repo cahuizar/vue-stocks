bf6bf7a966f958aa427c9856c5d7593a3158890a
Carlos Huizar <Carlos.Huizar@navient.com>
2020-05-01 16:14:36 -0400

    Create stocks component

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,41 @@
 <template>
-    <h1>The Stock - Stock Component</h1>
+    <div class="col-sm-6 col-md-4">
+        <div class="card">
+            <div class="card-header">
+                {{stock.name}}
+                <small>(Price: ${{stock.price}})</small>
+            </div>
+            <div class="card-body">
+                <div class="float-left">
+                    <input type="number" class="form-control" placeholder="Quantity" v-model="quantity">
+                </div>
+                <div class="float-right">
+                    <button class="btn btn-success" @click="buyStock" :disabled="quantity <= 0">Buy</button>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </template>
+
+<script>
+    export default {
+        props: ['stock'],
+        data() {
+            return {
+                quantity: 0
+            }
+        },
+        methods: {
+            buyStock(){
+                const order = {
+                    stockId: this.stock.id,
+                    stockPrice: this.stock.price,
+                    quantity: this.quantity
+                };
+                console.log(order);
+                this.quantity = 0;
+            }
+        }
+    }
+</script>
